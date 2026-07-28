7c152d73e51710931d0154ee9082e311cdd8dbbf
koki0809 <kouki20180728switch@gmail.com>
2026-07-28 13:41:20 +0900

    テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :orders
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :nickname, presence: true
   validates :password, presence: true, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
