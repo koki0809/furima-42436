83c8aed7e9bc9523128e8988ed0222c4c5693205
koki0809 <kouki20180728switch@gmail.com>
2026-09-01 14:34:20 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :active_storage_blobs
 end
--- db/migrate/20260901045155_create_items.rb
+++ db/migrate/20260901045155_create_items.rb
@@ -3,15 +3,15 @@ class CreateItems < ActiveRecord::Migration[7.1]
     create_table :items do |t|
 
       t.timestamps
-      t.string     :name,                  null:false
-      t.text       :description,           null:false
-      t.integer    :category_id,           nul:false
-      t.integer    :condition_id,          null:false
-      t.integer    :shipping_fee_burden_id null:false
-      t.integer    :shipping_origin_id     null:false
-      t.integer    :shipping_duration_id   null:false
-      t.integer    :price                  null:false
-      t.references :user                   null:false, foreign_key: true
+      t.string     :name,                   null:false
+      t.text       :description,            null:false
+      t.integer    :category_id,            null:false
+      t.integer    :condition_id,           null:false
+      t.integer    :shipping_fee_burden_id, null:false
+      t.integer    :shipping_origin_id,     null:false
+      t.integer    :shipping_duration_id,   null:false
+      t.integer    :price,                  null:false
+      t.references :user,                   null:false, foreign_key: true
     end
   end
 end
