f4497b79680454d07363d990fc9405454cf9a166
koki0809 <kouki20180728switch@gmail.com>
2026-09-03 15:22:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_duration
   belongs_to :shipping_fee_burden
   belongs_to :shipping_origin
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, numericality: { other_than: 1 }
+  validates :condition, numericality: { other_than: 1 }
+  validates :shipping_duration, numericality: { other_than: 1 }
+  validates :shipping_fee_burden, numericality: { other_than: 1 }
+  validates :shipping_origin, numericality: { other_than: 1 }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid" }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
