629b8f3afcb94a9426e8ec28b33da9eccb9381b9
koki0809 <kouki20180728switch@gmail.com>
2026-09-03 14:21:12 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :active_storage_blobs
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_duration
+  belongs_to :shipping_fee_burden
+  belongs_to :shipping_origin
 end
