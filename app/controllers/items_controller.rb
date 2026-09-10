6fa82090d67993b265d3def9804098e865dbf482
koki0809 <kouki20180728switch@gmail.com>
2026-09-10 14:51:37 +0900

    出品情報の保存機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
   def index
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = current_user.items.new(item_params)
+
+    if @item.save 
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+end
+
+private
+
+def item_params
+  params.require(:item).permit(
+    :name,
+    :image,
+    :description,
+    :category_id,
+    :condition_id,
+    :shipping_duration_id,
+    :shipping_fee_burden_id,
+    :shipping_origin_id,
+    :price
+  )
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
-  has_one_attached :active_storage_blobs
+  has_one_attached :image
+  
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_duration
@@ -9,10 +12,10 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category, numericality: { other_than: 1 }
-  validates :condition, numericality: { other_than: 1 }
-  validates :shipping_duration, numericality: { other_than: 1 }
-  validates :shipping_fee_burden, numericality: { other_than: 1 }
-  validates :shipping_origin, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_duration_id, numericality: { other_than: 1 }
+  validates :shipping_fee_burden_id, numericality: { other_than: 1 }
+  validates :shipping_origin_id, numericality: { other_than: 1 }
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid" }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
