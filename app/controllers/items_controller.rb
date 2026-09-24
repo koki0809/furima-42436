e5f6d17decd8cea934f2cd5636fdab606938d79b
koki0809 <kouki20180728switch@gmail.com>
2026-09-24 15:07:14 +0900

    -m　商品出品機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,21 +16,21 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end 
-end
 
-private
 
-def item_params
-  params.require(:item).permit(
-    :name,
-    :image,
-    :description,
-    :category_id,
-    :condition_id,
-    :shipping_duration_id,
-    :shipping_fee_burden_id,
-    :shipping_origin_id,
-    :price,
-    :image
-  )
+ private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :image,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_duration_id,
+      :shipping_fee_burden_id,
+      :shipping_origin_id,
+      :price
+    )
+  end
 end
